30cdfbef20c32f526dea92830b0a39d3565a5459
unknown <you@example.com>
2025-05-07 09:35:59 -0700

    Updated

--- Notes Task/finalwork.js
+++ Notes Task/finalwork.js
@@ -4,11 +4,13 @@ const addBtn = document.querySelector('#addBtn');
 const cancelBtn = document.querySelector('#cancelBtn');
 const output = document.querySelector('#output');
 const form = document.querySelector('form');
+const body = document.querySelector('body');
+const emptyNoteBook = document.createElement('h1');
 
-window.addEventListener('load', ()=>{
-    const emptyNoteBook = document.createElement('h1');
-    emptyNoteBook.textContent = 'Your Notebook is empty. You can add some notes.'
-    body.appendChild(emptyNoteBook);
+window.addEventListener('load', ()=>{ // Kai nera nei vieno priminimo, per viduri atsiranda uzrasas.
+        emptyNoteBook.textContent = 'Oops! Your Notebook is empty!...'
+        body.appendChild(emptyNoteBook);
+        emptyNoteBook.style.cssText = 'text-align: center; margin-top: 5em; color: red; font-family: normal; font-weight: 800; font-size: 24px';
 })
 
 addBtn.addEventListener('click', (event) => {
@@ -21,9 +23,7 @@ addBtn.addEventListener('click', (event) => {
             const emptyValue = document.createElement('p')
             emptyValue.setAttribute('id', 'error-message')
             emptyValue.textContent = 'Please write Title and Note to add them in your NoteBook!';
-            emptyValue.style.color = '#ff2200';
-            emptyValue.style.fontFamily = 'normal';
-            emptyValue.style.textAlign = 'center';
+            emptyValue.style.cssText = 'color: #ff2200; text-align: center; font-family: normal';
             form.append(emptyValue);
         }
     }
@@ -40,22 +40,13 @@ addBtn.addEventListener('click', (event) => {
         icon.setAttribute('class', 'fas fa-trash-alt');
         h4.textContent = titleValue;
         p.textContent = noteValue;
+        emptyNoteBook.remove();
 
         //Styliai div konteineriui
-        div.style.backgroundImage = 'url(https://t4.ftcdn.net/jpg/10/27/08/31/360_F_1027083125_CctAY0dbkVkVi1D8dYnoGJayzHtMmFR8.jpg)';
-        div.style.backgroundSize = 'cover';
-        div.style.display = 'flex';
-        div.style.flexDirection = 'column';
-        div.style.flexWrap = 'wrap';
-        div.style.backgroundPosition = 'center';
-        div.style.width = '200px';
-        div.style.height = '200px';
-        div.style.margin = '10px';
-        div.style.padding = '10px';
-        div.style.borderRadius = '7px';
-        div.style.boxShadow = '#7e3d33 0px 60px 40px -7px';
-        titleText.style.display = 'flex';
-        titleText.style.justifyContent = 'space-between';
+        div.style.cssText = 'background-image: url(https://t4.ftcdn.net/jpg/10/27/08/31/360_F_1027083125_CctAY0dbkVkVi1D8dYnoGJayzHtMmFR8.jpg); background-size: cover; display: flex; flex-direction: column; flex-wrap: wrap; background-position: center; width: 200px; height: 200px; margin: 10px; padding: 10px; border-radius: 7px; box-shadow: #7e3d33 0px 60px 40px -7px';
+        //CSS Title 
+        titleText.style.cssText = 'display: flex; justify-content: space-between;';
+        //CSS Ikonele(istrinti)
         icon.style.cursor = 'pointer';
         //Idedu viska i konteineri
         titleText.append(h4);
@@ -64,10 +55,30 @@ addBtn.addEventListener('click', (event) => {
         div.append(p);
         //Idedu viska i outputa
         output.append(div);
+        
+        let savedNotes = JSON.parse(localStorage.getItem('savedNotes') || '[]'); // sukuriu masyva localstorage ir saugoju tuos notes;
+        const noteId = Date.now();
+        savedNotes.push({
+            id: noteId,
+            title: titleValue,
+            note: noteValue
+        })
+
+        localStorage.setItem('savedNotes', JSON.stringify(savedNotes));
 
         icon.onclick = function() { // KAI PASPAUDZIU ANT IKONELES, ISTRINA NOTE IR PASALINA INFO IS LOCALSTORAGE
             div.remove();
-            const savedNotes = JSON.parse(localStorage.getItem('savedNotes') || '[]');
+            let savedNotes = JSON.parse(localStorage.getItem('savedNotes') || '[]');
+
+            savedNotes = savedNotes.filter(noteObj => noteObj.id !== noteId );
+            localStorage.setItem('savedNotes', JSON.stringify(savedNotes));
+
+            if(savedNotes.length === 0){
+                localStorage.removeItem('savedNotes');
+                body.appendChild(emptyNoteBook);
+            }else{
+                localStorage.setItem('savedNotes', JSON.stringify(savedNotes));
+            }
             
         }
 
@@ -79,24 +90,15 @@ addBtn.addEventListener('click', (event) => {
                 h4.style.textDecoration = 'none';
                 
             }else{
-                p.style.textDecoration = 'line-through';
-                p.style.textDecorationColor ='red';
-                p.style.textDecorationThickness = '5px';
-                h4.style.textDecoration = 'line-through';
-                h4.style.textDecorationColor ='red';
-                h4.style.textDecorationThickness = '5px';
+                p.style.cssText = 'text-decoration: line-through; text-decoration-color: red; text-decoration-thickness: 3px';
+                h4.style.cssText = 'text-decoration: line-through; text-decoration-color: red; text-decoration-thickness: 3px';
             }
         }
 
-        const savedNotes = JSON.parse(localStorage.getItem('savedNotes') || '[]');
-        
-        savedNotes.push({
-            title: titleValue,
-            note: noteValue
-        })
-
-        localStorage.setItem('savedNotes', JSON.stringify(savedNotes));
 
     }
+    // po paspaudimo, istrina texta inputuose, kad butu patogiau.
+    title.value = '';
+    note.value = '';
 
 })
