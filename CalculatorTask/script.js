c917445cf33f5c8ebb0c989776f9eefafa6c3f31
unknown <you@example.com>
2025-05-11 12:54:07 -0700

    Not completed

--- CalculatorTask/script.js
+++ CalculatorTask/script.js
@@ -1,1 +1,13 @@
 const calculator = document.querySelector('.calculator');
+const buttons = document.querySelectorAll('button');
+const display = document.querySelector('.display');
+
+
+buttons.forEach(button => {
+    button.addEventListener('click', () =>{
+        const btnValue = button.textContent;
+        display.textContent += btnValue;
+    })
+});
+
+
